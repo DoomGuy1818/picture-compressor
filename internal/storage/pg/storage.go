f2ea99721c36c5dca7a2f38cb54e05e16cf4f916
DoomGuy1818 <zasdqs745@gmail.com>
2026-01-25 14:06:47 +0300

    added migrations + edited logic for creating s3 vault + updated add-picture logic

--- internal/storage/pg/storage.go
+++ internal/storage/pg/storage.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 
+	"github.com/google/uuid"
 	_ "github.com/jackc/pgx/v5/stdlib" //pg driver init
 )
 
@@ -20,4 +21,46 @@ func New(storageURL string) (*Storage, error) {
 	}
 
 	return &Storage{db: db}, nil
+}
+
+func (s *Storage) AddPicture(id uuid.UUID, path string) error {
+	const op = "storage.pg.AddPicture"
+
+	tx, err := s.db.Begin()
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	defer func() {
+		if err != nil {
+			_ = tx.Rollback()
+		}
+	}()
+
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	_, err = tx.Exec(
+		"INSERT INTO pictures (id, file_path) VALUES ($1, $2)",
+		id,
+		path,
+	)
+	if err != nil {
+		return fmt.Errorf("%s: insert: %w", op, err)
+	}
+
+	if err = tx.Commit(); err != nil {
+		return fmt.Errorf("%s: commit: %w", op, err)
+	}
+
+	return nil
+}
+
+func (s *Storage) RemovePicture() {
+	const op = "storage.pg.RemovePicture"
+}
+
+func (s *Storage) GetCompressedPicture() {
+	const op = "storage.pg.GetCompressedPicture"
 }
